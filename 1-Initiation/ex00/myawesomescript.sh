c95986a3f72774af7409a7f074ae2d7b3329bd36
KobeLaC <g.beco015@gmail.com>
2025-03-28 15:29:41 +0100

    oui bon c est quand le php la

--- 1-Initiation/ex00/myawesomescript.sh
+++ 1-Initiation/ex00/myawesomescript.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [[ $1 == bit.ly/* ]]; then
+if [[ $1 = bit.ly/* ]]; then
         curl_response=$(curl -s -w "%{http_code}" "$1")
-        if [ ${curl_response: -3} == 301 ]; then
-            curl -s $1 | grep href | cut -d '"' -f2
+        if [ ${curl_response: -3} = 301 ]; then
+            curl -s $1 | grep href | cut -d '"' -f2 
         fi
 fi
